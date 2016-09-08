96795b1ea4672650a7c1cb37359a740bc65e46fa
Justin St. Arnauld <justin.starnauld@gmail.com>
2016-09-07 22:44:13 -0700

    Final static todos

--- server.js
+++ server.js
@@ -23,8 +23,6 @@ app.get('/todos', function (req, res) {
    filteredTodos = _.where(filteredTodos, {completed: false});
   }
 
-  // if q has property with length > 0
-
   if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
     filteredTodos = _.filter(filteredTodos, function (todo) {
       return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
