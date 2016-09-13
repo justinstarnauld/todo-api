b9ca7aa81b6a990a8910fe1d615c4eb2a69cc990
Justin St. Arnauld <justin.starnauld@gmail.com>
2016-09-12 22:55:59 -0700

    Convert CRUD requests to use authorization logic

--- server.js
+++ server.js
@@ -19,7 +19,9 @@ app.get('/', function (req, res) {
 // GET /todos?completed=true&q=work
 app.get('/todos', middleware.requireAuthentication, function (req, res) {
   var query = req.query;
-  var where = {};
+  var where = {
+    userId: req.user.get('id')
+  };
 
   if (query.hasOwnProperty('completed') && query.completed === 'true') {
    where.completed = true;
@@ -44,7 +46,13 @@ app.get('/todos', middleware.requireAuthentication, function (req, res) {
 app.get('/todos/:id', middleware.requireAuthentication, function (req, res) {
   var todoId = parseInt(req.params.id, 10);
 
-  db.todo.findById(todoId).then(function (todo) {
+  // change findById to findOne
+  db.todo.findOne({
+    where: {
+      id: todoId,
+      userId: req.user.get('id')
+    }
+  }).then(function (todo) {
     if (!!todo) {
       res.json(todo.toJSON());
     } else {
@@ -76,7 +84,8 @@ app.delete('/todos/:id', middleware.requireAuthentication, function (req, res) {
 
   db.todo.destroy({
     where: {
-      id: todoId
+      id: todoId,
+      userId: req.user.get('id')
     }
   }).then(function (rowsDeleted) {
     if (rowsDeleted === 0) {
@@ -106,7 +115,14 @@ app.put('/todos/:id', middleware.requireAuthentication, function (req, res) {
     attributes.completed = body.completed;
   }
 
-  db.todo.findById(todoId).then(function (todo) {
+  // user findOne and match todo ID
+
+  db.todo.findOne({
+    where: {
+      id: todoId,
+      userId: req.user.get('id')
+    }
+  }).then(function (todo) {
     if (!!todo) {
       todo.update(attributes).then(function (todo) {
         res.json(todo.toJSON());
@@ -151,7 +167,7 @@ app.post('/users/login', function (req, res) {
 });
 
 
-db.sequelize.sync({force: true}).then(function () {
+db.sequelize.sync().then(function () {
   app.listen(PORT, function () {
     console.log('Express listening on port ' + PORT + '!');
   });
